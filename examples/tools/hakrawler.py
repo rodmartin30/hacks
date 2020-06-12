ac103bd8c903b6c74459463653370b97920bd661
Martin Rodriguez <tinrodriguez8@gmail.com>
2020-06-11 22:00:25 -0300

    Added examples tools which helps to remember syntax of a command line tool.

---
+++ examples/tools/hakrawler.py
@@ -0,0 +1,14 @@
+{
+  "name":"hakrawler",
+  "link":"https://github.com/hakluke/hakrawler",
+  "short_desc":"hakrawler is a Go web crawler designed for easy, quick discovery of endpoints and assets within a web application. ",
+  "long_desc":"",
+  "helper":"",
+  "example":"hakrawler -plain -url rodmartin.me",
+  "examples": [
+      {
+        "desc":"",
+        "command":""
+      },
+  ]
+}
